09ce3802f71112f8847844ea2efcfa9ff390ca43
beautiful-inheritance-1081 <sduvly@gmail.com>
2021-04-12 22:11:48 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -3,28 +3,31 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
+lyric = "maybe";
+var lyric;
   return lyric;
-  var lyric = "maybe";
+
 }
 
 
 function crazy() {
-  // fix the code in here:
+  var thisIsCrazy;
   thisIsCrazy();
 
-  var thisIsCrazy = function (){
+  function thisIsCrazy(){
     console.log("hey!!!")
   }
 }
 
 function sayMyName() {
-  // fix the code in here:
+
   var name = "Cricky";
 
   sayMy();
 
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
+    
   }
 }
